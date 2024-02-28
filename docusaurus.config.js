1ffae21fb4b5638709a42cbeb011afd7d5a80f4d
Mohammed Essaid MEZERREG <me@mohessaid.com>
2024-02-28 12:11:32 +0100

    Updte baseUrl to use custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://ta9in.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/la9in',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
