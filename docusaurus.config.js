dd93e11dc0df949bfeae158b27f8e09803e7e1a9
Mohammed Essaid MEZERREG <me@mohessaid.com>
2024-02-28 11:36:29 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://la9in.ta9in.com',
+  url: 'https://ta9in.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -22,7 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Ta9in', // Usually your GitHub org/user name.
   projectName: 'la9in', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
